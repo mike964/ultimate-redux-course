5e5466ee4966da64735167638770b3c6452af50a
Mike <mike7olwo@gmail.com>
2022-03-30 12:23:15 +0300

    createSlice() from reduxjs/toolkit implemented

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,91 +1,33 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import {
+	// createAction, createReducer,
+	createSlice,
+} from '@reduxjs/toolkit'
 
-// Action Types
-const BUG_ADDED = 'bugAdded'
-const BUG_REMOVED = 'bugRemoved'
-const BUG_RESOLVED = 'bugResolved'
+let lastId = 0 //  last bug id
 
-// Action Creators
-// export const bugAdded = description => ({
-// 	type: BUG_ADDED,
-// 	payload: {
-// 		description,
-// 	},
-// })
-export const bugAdded = createAction('bugAdded')
-// console.log(bugAdded.type)
-
-// export const bugRemoved = id => ({
-// 	type: BUG_REMOVED,
-// 	payload: {
-// 		id,
-// 	},
-// })
-export const bugRemoved = createAction('bugRemoved')
-
-// export const bugResolved = id => ({
-// 	type: BUG_RESOLVED,
-// 	payload: {
-// 		id,
-// 	},
-// })
-export const bugResolved = createAction('bugResolved')
-
-// * REDUX TOOLKIT
-// const action = createAction('bugUpdated')
-// console.log(action)
-// console.log(action())
-
-// * Reducer
-let lastId = 0
-
-export default createReducer([], {
-	[bugAdded.type]: (bugs, action) => {
-		// bugs = state
-		bugs.push({
-			id: ++lastId,
-			description: action.payload.description,
-			resolved: false,
-		})
-	},
-	[bugResolved.type]: (bugs, action) => {
-		const index = bugs.findIndex(bug => bug.id === action.payload.id)
-		bugs[index].resolved = true
+// createSlice() automatically creates action types and action creators
+// which means we don't need to call createAction() and createReducer()
+// We need to export slice.reducer and actions
+const slice = createSlice({
+	name: 'bugs',
+	initialState: [],
+	reducers: {
+		// actions => action handlers
+		bugAdded: (bugs, action) => {
+			// bugs = state
+			bugs.push({
+				id: ++lastId,
+				description: action.payload.description,
+				resolved: false,
+			})
+		},
+		bugResolved: (bugs, action) => {
+			const index = bugs.findIndex(bug => bug.id === action.payload.id)
+			bugs[index].resolved = true
+		},
 	},
 })
+console.log(slice)
 
-// * OLD REDUCER (before createReducer)
-// export default function reducer(state = [], action) {
-// 	switch (action.type) {
-// 		// case BUG_ADDED:
-// 		case bugAdded.type:
-// 			return [
-// 				...state,
-// 				{
-// 					id: ++lastId,
-// 					description: action.payload.description,
-// 					resolved: false,
-// 				},
-// 			]
-// 		// case BUG_REMOVED:
-// 		case bugRemoved.type:
-// 			return state.filter(bug => bug.id !== action.payload.id)
-
-// 		// case BUG_RESOLVED:
-// 		case bugResolved.type:
-// 			return state.map(bug =>
-// 				bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-// 			)
-
-// 		default:
-// 			return state
-// 	}
-// }
-
-// # RULES of DUCK PATTERN
-/*
-- Reducer should be default export of module
-- 
-*/
-
-// HEELLLOOOO
+export const { bugAdded, bugResolved } = slice.actions
+export default slice.reducer
